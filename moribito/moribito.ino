06822baaf24842d5b03dd58ce1effd02d0464789
maedaU <my1866purin@gmail.com>
2017-01-18 18:06:39 +0900

    Update moribito.ino
    ArduinoからProcessingへのデータの受け渡しを可能にした
    スイッチを押したときの処理を変更
    　押したとき，人についていく か 家に帰るか のモードになる

--- moribito/moribito.ino
+++ moribito/moribito.ino
@@ -3,6 +3,7 @@ bool osw = HIGH;
 int mode_flag=0;
 
 void setup(){
+  Serial.begin(9600);
   pinMode(2,INPUT) ;    //スイッチに接続ピンをデジタル入力に設定
   pinMode(13, OUTPUT);  //ＬＥＤに接続ピンをデジタル出力に設定
   prev = millis();      //Arduinoボードがプログラムの実行を開始した時から現在までの時間をミリ秒単位で返します
@@ -10,56 +11,56 @@ void setup(){
 
 void loop(){
   unsigned long cur = millis();
-
+  if(Serial.available() > 0) {
+    mode_flag = Serial.read(); //Processingからのシリアルを読み込む，家に着いたかわかる
+  }
   if (cur - prev > 20) {
     bool sw = digitalRead(2);
     prev = cur;
     if (osw == LOW && sw == HIGH) { //スイッチを押して離したとき
-      mode_flag=mode_flag+1;
-      if(mode_flag>2)mode_flag=0; //最終的にmode_flagはついてくるか帰るかに変化
-      //if(mode_flag==1){
-      //  mode_flag=2;
-      //}else{
-      //  mode_flag=1;
-      //}
+      if(mode_flag==1){ //[1]人についていくときにボタンが押されたとき，[2]家に帰るモードに変更
+        mode_flag=2;
+      }else{            //[0]家にいるとき[2]家に帰るときにボタンを押されたとき，[1]人についていくモードに変更
+        mode_flag=1;
+      }
       delay(20);
+      //Serial.write(mode_flag); //Serialに書き込みProcessingに送る. mode_flagが  [0]家にいるとき，[1]人についていくとき，[2]家に帰るとき
     }
     osw = sw;
   }
-  mode(); //void mode()を実行
-
+  mode(); //void mode()を実行 arduinoの状態がわかるテスト用
 }
 
+//以下テスト用
 void mode(){//モードの選択
-  if (mode_flag==0) {  //家にいるとき
+  if (mode_flag==0) {  //[0]家にいるとき
     waiteHome();
   }
-  else if(mode_flag==1){//人についていくとき
+  else if(mode_flag==1){//[1]人についていくとき
     satellite();
   }
-  else if(mode_flag==2){//家に帰るとき
+  else if(mode_flag==2){//[2]家に帰るとき
     goHome();
   }
 }
 
-void waiteHome(){ //家にいるときの動作 mode_flag==0;
+void waiteHome(){ //[0]家にいるときの動作
   //仮code
-  digitalWrite(13, LOW); //消灯
+  Serial.write(0);
+  digitalWrite(13, LOW);
 }
 
-void satellite(){//人についていくときの動作 mode_flag==1;
+void satellite(){//[1]人についていくときの動作
   //仮code
-  digitalWrite(13, HIGH); //点灯
+  Serial.write(1);
+  digitalWrite(13, HIGH);
 }
 
-void goHome(){//家に帰るときの動作 mode_flag==2;
+void goHome(){//[2]家に帰るときの動作
   //仮code
-  digitalWrite(13, HIGH); //点滅
+  Serial.write(2);
+  digitalWrite(13, HIGH);
   delay(100);
   digitalWrite(13, LOW);
   delay(100);
-  
-  //家に着いたら　
-  //mode_flag=0;
 }
-
